5939be5f7822b4342905e4d10ca731af6916dab9
Daniel <danielftosta@hotmail.com>
2023-02-15 17:23:34 -0300

    s

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
 var Client = require('./Client');
-const http = require("http");
-const fs = require("fs");
-
-var clients = []
-const readFileLines = filename =>
-   fs.readFileSync(filename)
-   .toString('UTF8')
-   .split('\n');
-var proxies = readFileLines("./proxies.txt")
-for (let i = 0; i < proxies.length; i++) {proxies[i] = `http://${proxies[i]}`};
-
-proxies.forEach(p => {
-    const client = new Client("wss://www.multiplayerpiano.dev:8043", p);
-    client.setChannel("test/fishing");
-    client.start();
-    client.on('hi', message => {
-        clients.push(client)
-        client.setName("NMPB Proxy "+clients.length)
-    })
-});
-
-const getConnectedClients = () => clients.filter(c => c.isConnected());
-const nextClient = noteNumber => getConnectedClients()[Math.floor(noteNumber / (127 / getConnectedClients().length))];
+const client = new Client("wss://mpp.hri7566.info:8443", "");
 var keyNameMap = require('./key-map.json');
 var MidiPlayer = require('midi-player-js');
 var Player = new MidiPlayer.Player(function(event) {
@@ -30,20 +8,19 @@ var Player = new MidiPlayer.Player(function(event) {
         event.name == 'Note off' ||
         (event.name == 'Note on' && event.velocity === 0)
     ) {
-        nextClient(event.noteNumber).stopNote(keyNameMap[event.noteName]);
+        client.stopNote(keyNameMap[event.noteName]);
     } else if (event.name == 'Note on') {
-        nextClient(event.noteNumber).startNote(keyNameMap[event.noteName], event.velocity / 127); 
+        client.startNote(keyNameMap[event.noteName], event.velocity / 127); 
     } else if (event.name == 'Set Tempo') {
         Player.setTempo(event.data);
     }
 });
 
-const client2 = new Client("wss://www.multiplayerpiano.dev:8043", null);
-client2.setChannel("test/fishing");
-client2.start();
-client2.on('hi',() => {
-    setTimeout(function(){
-        Player.loadFile("D:/midis/BlackMidis/[BLACK MIDI] jinjenia redzone black.mid");
-        Player.play();
-    },1000)
+
+client.on('hi',() => {
+    console.log("hi")
+    client.setChannel("lobby");
+    Player.loadFile("D:/midis/BlackMidis/[BLACK MIDI] The Nuker 2 audio final fix Anon64.mid");
+    Player.play();
 })
+client.start();
